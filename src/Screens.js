a305f6aeeaf03acb8d5578ac7d6896a430d156f3
m4gicpur22 <affner@knights.ucf.edu>
2020-12-17 03:05:16 -0500

    Adding in yelp API

--- src/Screens.js
+++ src/Screens.js
@@ -2,18 +2,42 @@ import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from './Components/SearchBar';
 
+import yelpKey from './API/yelp';
+
+
 const Screens = () => {
-    const [Search, setSearch] = useState('');
+    const [search, setSearch] = useState('');
+    const [results, setResults] = useState([]);
+    const [errormssg, setErrormssg] = useState('');
+
+    const yelpApi = async (searchTerm) => {
+
+        try {
+            const res = await yelpKey.get('/search', {
+                params: {
+                    limit: 50,
+                    search: searchTerm,
+                    location: 'orlando'
+                }
+            });
+            setResults(res.data.businesses);
+        }
+        catch(err){
+            console.log("Error with api call of: " + err);
+            setErrormssg('Error with yelp API call');
+        };
+    }
 
     return (
         <View>
             <SearchBar 
-            search={Search} 
-            onSearchChange={newSearch => setSearch(newSearch)}
-            onSearchSubmit={ () => console.log("Submitted!")}
+                search={search} 
+                onSearchChange={setSearch}
+                onSearchSubmit={() => yelpApi(search)}
             />
+            {errormssg ? <Text>{errormssg}</Text>:null}
             <Text>Search Screen!</Text>
-            <Text>{Search}</Text>
+            <Text>We have found {results.length} results</Text>
         </View>
     );
 }
